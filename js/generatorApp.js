0418be6c86106189a638957ae7c38061d61029c4
arfitale <arfitale@gmail.com>
2021-10-17 11:07:29 +0700

    add tooltip features

--- js/generatorApp.js
+++ js/generatorApp.js
@@ -4,11 +4,20 @@ import {hook} from './modules/hook.js';
 const COLOR_FIELD = hook(".color-field");
 const TOOLS = hook(".tools");
 const NAV = hook(".nav");
+const tippy = window.tippy;
 
 // TOOLS
 const generateBtn = hook(".btn-generate");
 
+
+// MODULE
+function setTooltip() {
+    tippy("button");
+    return;
+}
+
 function _init() {
+    setTooltip();
     generator();
 }
 
@@ -19,6 +28,9 @@ document.addEventListener("DOMContentLoaded", e => {
 
 // generate button
 generateBtn.addEventListener("click", generator);
+generateBtn.addEventListener("dragstart", e => {
+
+});
 
 window.addEventListener("keydown", e => {
     // spacebar button
